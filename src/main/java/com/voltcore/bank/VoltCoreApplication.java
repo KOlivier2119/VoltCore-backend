de7cc010acf727390f51f3120281a7fd6082e481
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2025-06-01 14:28:37 +0200

    Initial commit

---
+++ src/main/java/com/voltcore/bank/VoltCoreApplication.java
@@ -0,0 +1,13 @@
+package com.voltcore.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoltCoreApplication.class, args);
+    }
+
+}
